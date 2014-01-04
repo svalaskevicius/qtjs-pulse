eefd8c2547295daf6d878d461da2a15a5b290f4b
Sarunas Valaskevicius <rakatan@gmail.com>
2014-01-04 16:10:45 +0000

    refactor state matcher

--- src/highlighter/textProcessor.js
+++ src/highlighter/textProcessor.js
@@ -6,25 +6,34 @@ var TextProcessor = function() {
     this.states = []
 }
 
+var matchState = function(text, state) {
+    state.start.lastIndex = 0
+    if (state.start.test(text)) {
+        return state.start.lastIndex
+    }
+    return false
+}
+
+var findNextState = function(text, states) {
+    return _.reduce(
+        states,
+        function (prev, state) {
+            var idx = matchState(text, state)
+            if (idx !== false && idx < prev.start) {
+                return {state: state, start: state.start.lastIndex}
+            }
+            return prev
+        },
+        {state:undefined, start:text.length + 1}
+    )
+}
+
 TextProcessor.prototype = {
     'addState' : function(state) {
         this.states.push(state)
     },
     'processLine' : function(text, stateStack) {
-        var lastState = _.last(stateStack)
-        var newState = _.reduce(
-            this.states,
-            function (prev, state) {
-                state.start.lastIndex = 0
-                if (state.start.test(text)) {
-                    if (state.start.lastIndex < prev.start) {
-                        return {state: state, start: state.start.lastIndex}
-                    }
-                }
-                return prev
-            },
-            {state:lastState, start:text.length + 1}
-        )
+        var newState = findNextState(text, this.states)
         if (newState.state) {
             stateStack.push(newState.state.id)
         }
