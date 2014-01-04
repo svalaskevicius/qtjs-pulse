84c62a1a47c93ba6e7c84aa10664728fb6a1ee2b
Sarunas Valaskevicius <rakatan@gmail.com>
2014-01-04 21:36:07 +0000

    implement rule prioritisation

--- src/highlighter/ruleProcessor.js
+++ src/highlighter/ruleProcessor.js
@@ -1,13 +1,39 @@
 "use strict";
 
+var _ = require("lodash")
+
 var RuleProcessor = function(callback) {
     this.callback = callback
 }
 
+var matchRuleInRange = function(text, matcher, range, callback) {
+    var start = range[0]
+    var end = range[1]
+
+    matcher.lastIndex = start
+    while (true) {
+        var result = matcher.exec(text)
+        if (result && result.index + result[0].length <= end) {
+            callback(result.index, result.index + result[0].length)
+        } else {
+            return
+        }
+    }
+}
 
 RuleProcessor.prototype = {
     'processRules' : function(text, rules, start, end) {
-        this.callback('t-word', 3, 6)
+        var ranges = [[start, end]]
+        var callback = this.callback
+        _.forEach(rules, function(rule){
+            for (var r = ranges.length-1; r >= 0; r--) {
+                matchRuleInRange(text, rule.matcher, ranges[r], function(start, end){
+                    callback(rule.id, start, end)
+                    ranges.push([end, ranges[r][1]])
+                    ranges[r][1] = start
+                })
+            }
+        })
     }
 }
 
--- test/highlighter/ruleProcessor.js
+++ test/highlighter/ruleProcessor.js
@@ -1,7 +1,6 @@
 "use strict"
 
-var should = require('should'),
-    sinon = require('sinon')
+var should = require('should'), sinon = require('sinon')
 
 var RuleProcessor = require("../../src/highlighter/ruleProcessor.js")
 
@@ -16,9 +15,25 @@ describe('Highlighter/RuleProcessor', function () {
                                                         matcher: /\bt[a-z0-9_]*\b/gi
                                                     }], 0, 12)
 
-
             callback.calledOnce.should.equal(true)
-            callback.getCall(0).args.should.eql(["t-word", 3, 6])
+            callback.getCall(0).args.should.eql(["t-word", 3, 7])
+        })
+
+        it('prioritises rules by their order', function () {
+            var callback = sinon.spy()
+
+            var processor = new RuleProcessor(callback)
+            processor.processRules("my text t_line", [{
+                                                        id: 't-word-4',
+                                                        matcher: /\bt[a-z0-9_]{3}\b/gi
+                                                    }, {
+                                                          id: 't-word',
+                                                          matcher: /\bt[a-z0-9_]*\b/gi
+                                                    }], 0, 14)
+
+            callback.callCount.should.equal(2)
+            callback.getCall(0).args.should.eql(["t-word-4", 3, 7])
+            callback.getCall(1).args.should.eql(["t-word", 8, 14])
         })
     })
 })
