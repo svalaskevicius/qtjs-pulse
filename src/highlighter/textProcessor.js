9dab6d657a43bea204184ef58d3b1ded403e187f
Sarunas Valaskevicius <rakatan@gmail.com>
2014-01-04 15:51:33 +0000

    match new states

--- src/highlighter/textProcessor.js
+++ src/highlighter/textProcessor.js
@@ -1,12 +1,34 @@
 "use strict";
 
-var TextProcessor = function() {
+var _ = require('lodash')
 
+var TextProcessor = function() {
+    this.states = []
 }
 
 TextProcessor.prototype = {
-    'processLine' : function(text, state) {
-        return state
+    'addState' : function(state) {
+        this.states.push(state)
+    },
+    'processLine' : function(text, stateStack) {
+        var lastState = _.last(stateStack)
+        var newState = _.reduce(
+            this.states,
+            function (prev, state) {
+                state.start.lastIndex = 0
+                if (state.start.test(text)) {
+                    if (state.start.lastIndex < prev.start) {
+                        return {state: state, start: state.start.lastIndex}
+                    }
+                }
+                return prev
+            },
+            {state:lastState, start:text.length + 1}
+        )
+        if (newState.state) {
+            stateStack.push(newState.state.id)
+        }
+        return stateStack
     }
 }
 
--- test/highlighter/textProcessor.js
+++ test/highlighter/textProcessor.js
@@ -8,8 +8,18 @@ describe('Highlighter/textProcessor', function () {
     describe('#processLine()', function () {
         it('returns the same state when there are no rules', function () {
             var processor = new TextProcessor()
-            var state = {}
+            var state = []
             processor.processLine("my text line", state).should.equal(state)
+        })
+
+        it('changes state when a rule is triggered', function () {
+            var processor = new TextProcessor()
+            processor.addState({
+                'id' : 'test_state',
+                'start' : /\bt/g,
+                'end' : /not found\b/,
+            })
+            processor.processLine("my text line", []).should.eql(['test_state'])
         })
     })
 })
