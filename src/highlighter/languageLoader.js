7d54e198ca0a83768fe7197eb611063b4f53f667
Sarunas Valaskevicius <rakatan@gmail.com>
2014-06-07 20:44:22 +0100

    be less mutative

--- src/highlighter/languageLoader.js
+++ src/highlighter/languageLoader.js
@@ -8,32 +8,35 @@ import {TextProcessor} from './textProcessor';
 
 function prepareRegexp(state, name)
 {
+    var newState = _.clone(state);
     if (typeof state[name] !== 'undefined') {
         var flags = ""
         var flagsName = name+"-flags"
         if (typeof state[flagsName] !== 'undefined') {
             flags = state[flagsName]
-            delete state[flagsName]
+            delete newState[flagsName]
         }
 
         if (flags.indexOf("g") < 0) {
             flags += "g"
         }
 
-        state[name] = new RegExp(state[name], flags)
+        newState[name] = new RegExp(state[name], flags)
     }
-    return state
+    return newState
 }
 
 function prepareStateRulesMatchers(state)
 {
+    var newState = _.clone(state);
     if (state.rules) {
-        state.rules = _.map(state.rules, (rule, id) => {
-            rule.id = id
-            prepareRegexp(rule, "matcher")
-            return rule
+        newState.rules = _.map(state.rules, (rule, id) => {
+            var newRule = prepareRegexp(rule, "matcher")
+            newRule.id = id
+            return newRule
         })
     }
+    return newState
 }
 
 @Inject(TextProcessor)
@@ -43,10 +46,10 @@ export class LanguageLoader {
     }
 
     load(name, states) {
-        _.forEach(_.cloneDeep(states), (state, id) => {
-            prepareStateRulesMatchers(state)
-            prepareRegexp(state, "start")
-            prepareRegexp(state, "end")
+        _.forEach(states, (state, id) => {
+            state = prepareStateRulesMatchers(state)
+            state = prepareRegexp(state, "start")
+            state = prepareRegexp(state, "end")
             state.id = id
             this.target.addState(state)
         })
